8854542a0d5948991447f94f55f9792964d7fc7c
Andre Uschmann <mail@andre-uschmann.de>
2015-08-13 13:50:46 +0200

    UPDATED: Enclosed app module in anonymous function ADDED: AppController to app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
-angular.module('app', [
-                       'ngAnimate',
-                       'ngCookies',
-                       'ngResource',
-                       'ngRoute'
-                       ]);
+(function() {
+    var module = angular.module('app', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute'
+    ]);
+
+    module.controller('AppController', ['$scope', function($scope) {
+        $scope.title = 'Hello World';
+    }]);
+})();
+
